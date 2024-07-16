4de321840034c6a4337167dc4ae64ee44041d07e
Mani <manicse786@gmail.com>
2024-07-16 17:47:13 +0530

    wordtopdf

---
+++ src/main/java/com/pdf/word/PdfWordApplication.java
@@ -0,0 +1,15 @@
+package com.pdf.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfWordApplication.class, args);
+	}
+
+}
+//How to convert word file to pdf
+//How to convert pdf to word 
